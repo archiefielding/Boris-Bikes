b32d887bdac475ed3f13cefe2353f8fb0c88d608
Archie Fielding <archiefielding@gmail.com>
2020-05-23 18:30:39 +0800

    Changed capacity to 20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -14,31 +14,25 @@ describe DockingStation do
 
   it { is_expected.to respond_to :bike }
 
-describe 'dock' do
+describe '#dock' do
   it { is_expected.to respond_to(:dock).with(1).argument }
 
   it 'docks something' do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
-  end
-
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+    expect(subject.dock(bike)).to eq [bike]
   end
 
     it 'raises an error when full' do
-      subject.dock(Bike.new)
+      20.times { subject.dock(Bike.new) }
       expect { subject.dock(Bike.new) }.to raise_error 'Docking station full.'
     end
 end
 
-  describe 'release_bike' do
+  describe '#release_bike' do
     it 'releases a bike' do
       bike = Bike.new
       subject.dock(bike)
-      expect(subject.bike).to eq bike
+      expect(subject.release_bike).to eq bike
     end
 
     it 'raises an error when there are no bikes available' do
